ee73666f6e7c2f0f5f2be67324652451ecc3a706
Viet Nguyen <nguyentamquocviet@vus-etsc.edu.vn>
2024-12-20 13:55:19 +0700

    Update snowflake

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () => {
         <div className="carousel-content">
           <h1 className="carousel-title">It's Christmas Time Now</h1>
           <p className="carousel-subtitle">
-            <b>Anh chúc Tài thêm 1 mùa giáng sinh vui vẻ và ấm áp bên người thân yêu thương của mình nhé!</b>
+            <b>Anh chúc Tài thêm một mùa Giáng sinh vui vẻ và ấm áp bên người thân yêu thương của mình nhé!</b>
           </p>
         </div>
       </div>
