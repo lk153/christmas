48629a9f7eb2cb29234e5c22c511203955bbb389
Viet Nguyen <nguyentamquocviet@vus-etsc.edu.vn>
2024-12-20 11:43:48 +0700

    Init Christmas site

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="landing-page">
+      <div className="carousel">
+        <div className="carousel-content">
+          <h1 className="carousel-title">It's Christmas Time Now</h1>
+          <p className="carousel-subtitle">
+            <b>Anh chúc Tài thêm 1 mùa giáng sinh vui vẻ và ấm áp bên người thân yêu thương của mình nhé!</b>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
