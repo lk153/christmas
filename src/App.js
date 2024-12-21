639a6f42e8a8c5931a223f0f54433bb4cbbdf76c
Viet Nguyen <nguyentamquocviet@vus-etsc.edu.vn>
2024-12-21 20:06:56 +0700

    Add Jingle Bell

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./App.css";
 
 const App = () => {
+  const audioRef = useRef(null);
+
+  const handlePlay = () => {
+    const audio = audioRef.current;
+    if (audio) {
+      audio.play();
+    }
+  };
+
   return (
     <div className="landing-page">
+      {/* Background Audio */}
+      <audio ref={audioRef} src={process.env.PUBLIC_URL + '/christmas.mp3'} loop />
+
+      {/* Jingle Bell Button */}
+      <button onClick={handlePlay} className="jingle-bell-button">
+        <span className="bell-icon">🔔</span>
+      </button>
       <div className="carousel">
         <div className="carousel-content">
           <h1 className="carousel-title">It's Christmas Time Now</h1>
